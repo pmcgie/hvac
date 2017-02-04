dcd2a8fbf7fe7661288707c2508e46c05290a912
clara <clara@clara.com>
2017-02-03 18:00:05 -0600

    Removed redundant code

--- src/com/company/Furnace.java
+++ src/com/company/Furnace.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 public class Furnace extends ServiceCall {
 
+    /* An enum is a group of constants. Since the furnace's type must be one of these, use
+    an Enum to contain the allowed types.  */
     enum FurnaceType {
         FORCED_AIR,
         BOILER,
@@ -38,58 +40,4 @@ public class Furnace extends ServiceCall {
                 "Fee = " + feeString ;
 
     }
-
-
-    //Inner class to collect the different varieties of furnace
-    //When we ask for the type, we'll use the constants from this class to display the choice of types.
-
-//    protected static class FurnaceTypeManager {
-//
-//        protected static final int FORCED_AIR = 1;
-//        protected static final int BOILER = 2;
-//        protected static final int GRAVITY = 3;
-//
-//        static HashMap<Integer, String> furnaceTypes;
-//
-//        //Static initialization block
-//        static {
-//            furnaceTypes = new HashMap<Integer, String>();
-//            furnaceTypes.put(FORCED_AIR,"Forced Air");
-//            furnaceTypes.put(BOILER, "Boiler/Radiators");
-//            furnaceTypes.put(GRAVITY, "Gravity Feed (the older 'octopus' style)");
-//        }
-//
-//        public static String getTypeString(int typeInt) {
-//
-//            if (furnaceTypes.containsKey(typeInt)) {
-//                return furnaceTypes.get(typeInt);
-//            }
-//            else {
-//                return "Unknown type";
-//            }
-//
-//        }
-//
-//        public static String furnaceTypeUserChoices() {
-//
-//            //Get all of the keys from the HashMap and sort them in order
-//            ArrayList<Integer> keys = new ArrayList<Integer>(furnaceTypes.keySet());
-//            Collections.sort(keys);
-//
-//
-//            //Build and return a string of all the keys and their values
-//            String userChoices = "";
-//            for (Integer k : keys) {
-//
-//                userChoices = userChoices + k + " : " + furnaceTypes.get(k) + "\n";
-//
-//            }
-//
-//            return userChoices;
-//        }
-//
-//    }
-
-
-
 }
